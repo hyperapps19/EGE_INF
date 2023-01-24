bf6209713a626f41fefa33b0ef4481533cee3ddd
SkreckOne <63405052+SkreckOne@users.noreply.github.com>
2023-01-24 21:15:36 +0700

    Add files via upload

--- 22/time_process.py
+++ 22/time_process.py
@@ -1,5 +1,6 @@
 from collections import deque
 from csv import reader
+from copy import deepcopy
 
 def fill_tree(maindeq, maintree):
     while len(maindeq) > 0:
@@ -17,21 +18,33 @@ def fill_tree(maindeq, maintree):
             else:
                 maintree[pid] = (time + max(dep_times), deps)
 
+
+def get_min_time(maintree):
+    m = -1
+    for i in maintree:
+        m = max(maintree[i][0], m)
+
+    return m
+
 maindeq = deque()
 maintree = dict()
 
-
-with open("йцййц.csv") as f:
+with open("22-32.csv") as f:
     s = reader(f, delimiter=';', quotechar='"')
     next(s)
     for line in f:
         sp = line.strip().replace("\"", "").replace(" ", "").split(";")
-        maindeq.append((int(sp[0]), int(sp[1]), [int(i) for i in sp[2:]]))
+        maindeq.append([int(sp[0]), int(sp[1]), [int(i) for i in sp[2:]]])
 
-fill_tree(maindeq, maintree)
+copy_deq = deepcopy(maindeq)
+# время меняется у 13 пида
+for i in range(1, 100):
+    maindeq[15][1] = i # здесь ОЧЕНЬ ВНИМАТЕЛЬНО, там где первые скобки убедитесь что это именно нужный инжекс
+    fill_tree(maindeq, maintree)
+    if get_min_time(maintree) >= 134:
+        print(i)
+        break
+    else:
+        maindeq = deepcopy(copy_deq)
+        maintree = dict()
 
-m = -1
-for i in maintree:
-    m = max(maintree[i][0], m)
-
-print(m)
